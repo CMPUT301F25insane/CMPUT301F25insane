e654ff7d98bcfff6657c12c40865764198cf6435
RamizHHH <RamizHussain500@Gmail.com>
2025-10-21 21:51:14 -0600

    Fixed naming

--- code/app/src/main/java/com/example/camaraderie/LoginActivity.java
+++ code/app/src/main/java/com/example/camaraderie/LoginActivity.java
@@ -8,11 +8,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LogInActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.login);
+        setContentView(R.layout.login_activity);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
