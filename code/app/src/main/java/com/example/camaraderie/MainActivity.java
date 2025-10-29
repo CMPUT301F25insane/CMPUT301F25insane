420fc05a775ef0c9583fd5e63f804c5803eb51a0
RamizHHH <RamizHussain500@Gmail.com>
2025-10-28 21:24:36 -0600

    Cool

--- code/app/src/main/java/com/example/camaraderie/MainActivity.java
+++ code/app/src/main/java/com/example/camaraderie/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.camaraderie;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseFirestore db;
 
     static private CollectionReference eventsRef;
+    boolean userExists = false;
     static private CollectionReference usersRef;
 
     @Override
@@ -36,9 +42,45 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        String id = Settings.Secure.ANDROID_ID;
+
+        db = FirebaseFirestore.getInstance();
+        usersRef = db.collection("Users");
+
+        usersRef.whereEqualTo("UserID", id ).get().addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                if(!task.getResult().isEmpty()){
+                    for(QueryDocumentSnapshot documentSnapshot : task.getResult()){
+                        Log.d("Firestore", "Found Document");
+                        userExists = true;
+                    }
+                }
+                else{
+                    Log.d("Firestore", "No Documents");
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    LayoutInflater inflater = getLayoutInflater();
+                    View dialogView = inflater.inflate(R.layout.user_info_dialog, null);
+                    EditText name = findViewById(R.id.name_edit_text);
+                    EditText Email = findViewById(R.id.editTextTextEmailAddress);
+                    EditText address = findViewById(R.id.editTextTextPostalAddress);
+                    EditText phoneNum = findViewById(R.id.editTextPhone);
+
+                    builder.setMessage("Please enter Your information to create a profile")
+                            .setPositiveButton("Done", (dialog, id1)->{
+
+                            })
+                    userExists = false;
+                }
+            }
+            else{
+                Log.d("Firestore", "Did not get documents");
+            }
+        });
+
         db = FirebaseFirestore.getInstance();
         eventsRef = db.collection("Events");
         usersRef = db.collection("Users");
 
     }
+
 }
