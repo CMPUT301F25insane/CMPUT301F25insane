68ff29b50092e97da203bbba1ede09b08c3ea6bf
Tahmid Parvez <tahmidparvez343@gmail.com>
2025-10-29 13:29:44 -0600

    rough xml created for CreatedEventFragment. Need to refine and make some buttons. Need to figure the QR code stuff.

--- code/app/src/main/java/com/example/camaraderie/CreatedEventFragment.java
+++ code/app/src/main/java/com/example/camaraderie/CreatedEventFragment.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CreatedEventActivity extends AppCompatActivity {
+public class CreatedEventFragment extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
