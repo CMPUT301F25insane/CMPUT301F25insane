85349034ee13a034e1908a899e173e1b1a5413e5
Tahmid-Parvez <tahmidparvez343@gmail.com>
2025-11-01 12:57:14 -0600

    CreatedEventFragment: - added user join/unjoin functionality

--- code/app/src/main/java/com/example/camaraderie/CreatedEventFragment.java
+++ code/app/src/main/java/com/example/camaraderie/CreatedEventFragment.java
@@ -1,28 +1,20 @@
 package com.example.camaraderie;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-import androidx.annotation.ContentView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import com.example.camaraderie.databinding.CreatedEventBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 
 public class CreatedEventFragment extends Fragment {
 
@@ -32,8 +24,6 @@ public class CreatedEventFragment extends Fragment {
     private DocumentReference event;
     private DocumentReference user;
 
-    private CollectionReference eventsRef;
-    private CollectionReference usersRef;
     private static final String ARG_EVENT = "event";
     private static final String ARG_USER = "user";
 
@@ -46,12 +36,12 @@ public class CreatedEventFragment extends Fragment {
     private DocumentReference waitlistDocRef;
     private String hostName;
 
-    // Factory method to create a new instance with Event
-    public static CreatedEventFragment newInstance(String eventID, String userID) {
+    public static CreatedEventFragment newInstance(DocumentReference event, DocumentReference user) {
         CreatedEventFragment fragment = new CreatedEventFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_EVENT, eventID);
-        args.putString(ARG_USER, userID);
+
+        args.putString(ARG_EVENT, event.getPath());
+        args.putString(ARG_USER, user.getPath());
         fragment.setArguments(args);
         return fragment;
     }
@@ -70,26 +60,35 @@ public class CreatedEventFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        String eventString;
-        String userString;
-        eventString = (String) getArguments().getSerializable(ARG_EVENT);
-        userString =(String)  getArguments().getSerializable(ARG_USER);
+        String eventPath;
+        String userPath;
+        eventPath = (String) getArguments().getSerializable(ARG_EVENT);
+        userPath = (String)  getArguments().getSerializable(ARG_USER);
 
         db = FirebaseFirestore.getInstance();
-        eventsRef = db.collection("Events");
-        usersRef = db.collection("Users");
-
-        user = usersRef.document(userString);
-        event = eventsRef.document(eventString);
+        event = db.document(eventPath);
+        user = db.document(userPath);
 
         fillTextViews(event);
 
         binding.joinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /**
-                 * adding the user to the waitlist document
-                 */
+
+                waitlistDocRef.get().addOnCompleteListener( task -> {
+                    if (task.isSuccessful()){
+                        DocumentSnapshot userToAdd = task.getResult();
+                        if (userToAdd.exists()){
+                            DocumentReference existingUser = user;
+                            existingUser.delete();
+                        }
+                        else{
+                            waitlistDocRef.set(userToAdd);
+                        }
+                    }
+                });
+
+
             }
         });
     }
