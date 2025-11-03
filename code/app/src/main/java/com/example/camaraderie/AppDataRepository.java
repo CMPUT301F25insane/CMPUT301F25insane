6d657654b7c24aefcbdf0cc105ec3a9e2c2530b6
Tahmid-Parvez <tahmidparvez343@gmail.com>
2025-11-02 22:13:15 -0700

    AppDataRepository changes

--- code/app/src/main/java/com/example/camaraderie/AppDataRepository.java
+++ code/app/src/main/java/com/example/camaraderie/AppDataRepository.java
@@ -2,28 +2,23 @@ package com.example.camaraderie;
 
 import com.google.firebase.firestore.DocumentReference;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class AppDataRepository {
-    private String sharedData;
-    private DocumentReference sharedDocRef;
+    private String userDocPath;
 
     public String getSharedData() {
-        return sharedData;
+        return userDocPath;
     }
 
     public void setSharedData(String data) {
-        this.sharedData = data;
+        this.userDocPath = data;
     }
 
-    public DocumentReference getSharedDocRef() {
-        return sharedDocRef;
-    }
-
-    public void setSharedDocRef(DocumentReference docRef) {
-        this.sharedDocRef = docRef;
-    }
+    @Inject
+    public AppDataRepository(){};
 
 
 }
