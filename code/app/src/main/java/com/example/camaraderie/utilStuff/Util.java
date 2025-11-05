f94608fb52ed4e6a15a707970f6edfb62b2b5b37
Fecici <abouchti2005@gmail.com>
2025-11-04 18:49:51 -0700

    added to util class

--- code/app/src/main/java/com/example/camaraderie/utilStuff/Util.java
+++ code/app/src/main/java/com/example/camaraderie/utilStuff/Util.java
@@ -1,9 +1,14 @@
 package com.example.camaraderie.utilStuff;//
 
+import android.util.Log;
+
 import com.example.camaraderie.Event;
 import com.example.camaraderie.User;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.WriteBatch;
 
 import java.util.Date;
 
@@ -49,5 +54,49 @@ public class Util {
 
             System.out.println("added user and event " + i);
         }
+    }
+
+    public static void clearUsersCollection() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Users")
+                .get()
+                .addOnSuccessListener(snapshot -> {
+                    WriteBatch batch = db.batch();
+                    for (DocumentSnapshot doc : snapshot) {
+                        batch.delete(doc.getReference());
+                    }
+                    batch.commit()
+                            .addOnSuccessListener(aVoid -> Log.d("Firestore", "All Users deleted"))
+                            .addOnFailureListener(e -> Log.e("Firestore", "Error deleting Users", e));
+                });
+    }
+
+    public static void clearEventsCollection() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Events")
+                .get()
+                .addOnSuccessListener(snapshot -> {
+                    WriteBatch batch = db.batch();
+                    for (DocumentSnapshot doc : snapshot) {
+                        batch.delete(doc.getReference());
+                    }
+                    batch.commit()
+                            .addOnSuccessListener(aVoid -> Log.d("Firestore", "All Events deleted"))
+                            .addOnFailureListener(e -> Log.e("Firestore", "Error deleting Events", e));
+                });
+
+
+    }
+
+    public static void clearDB() {
+        clearEventsCollection();
+        clearUsersCollection();
+    }
+
+    public static void setUserAsAdmin(DocumentReference user, boolean isAdmin) {
+        String msg = "isAdmin for User " + user + " set to " + isAdmin;
+        user.update("isAdmin", isAdmin)
+                .addOnSuccessListener(aVoid -> Log.d("Firestore", msg))
+                .addOnFailureListener(e -> Log.e("Firestore", "Error setting user" + user + " to " + isAdmin));
     }
 }
