8dcc235295214acf2f8f7b633f6b23a486ba51cd
CollinsMutea <103341395+CollinsMutea@users.noreply.github.com>
2025-11-26 19:12:59 -0700

    Rewrote the tests for DeleteUserRemovesEventTest.java, EventUnitTest, UserUnitTest.java and ViewWaitlistFragmentTest

--- code/app/src/test/java/com/example/camaraderie/DeleteUserRemovesEventTest.java
+++ code/app/src/test/java/com/example/camaraderie/DeleteUserRemovesEventTest.java
@@ -1,52 +1,69 @@
 package com.example.camaraderie;
 
+import static com.example.camaraderie.main.MainActivity.user;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
+import android.net.Uri;
 
+import com.example.camaraderie.updateUserStuff.UpdateUserFragment;
+import com.example.camaraderie.utilStuff.UserDeleter;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.SetOptions;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Calendar;
 import java.util.Date;
-
 /**
- *Here are ensuring that deleting a User removes them from an Event they joined.
+ * Unit tests for the Event class.
+ *
  */
+
 
 public class DeleteUserRemovesEventTest {
     private Event event;
     private DocumentReference mockHost;
     private DocumentReference mockEventRef;
     private DocumentReference mockUser1;
+    private final Calendar cal = Calendar.getInstance();
+    private UserDeleter deleter;
 
     @Before
     public void setUp() {
+        cal.set(2025, Calendar.DECEMBER, 5);
+        Date registrationDeadline = cal.getTime();
+
+        cal.set(2025, Calendar.DECEMBER, 12);
+        Date eventDate = cal.getTime();
+
         mockHost = mock(DocumentReference.class);
         mockEventRef = mock(DocumentReference.class);
         mockUser1 = mock(DocumentReference.class);
-
         event = new Event(
                 "Hackathon",
                 "Edmonton Hall",
-                new Date(),
+                registrationDeadline,
                 "24-hour coding competition",
-                new Date(),
+                eventDate,
                 "09:00 AM",
+                2,
                 2,
                 mockHost,
                 mockEventRef,
-                "E123"
+                "E123",
+                null,
+                false
         );
     }
 
     @Test
-    public void testAddWaitlistUser() {
+    public void RemoveUserTest() {
         event.addWaitlistUser(mockUser1);
         assertTrue(event.getWaitlist().contains(mockUser1));
-
+        deleter.DeleteUser((Runnable) mockUser1);
         assertFalse(event.getWaitlist().contains(mockUser1));
     }
 }
