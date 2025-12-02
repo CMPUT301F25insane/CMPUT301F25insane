ee7aadb205eeb7dc1998a33d2120bd288c40b9ae
RamizHHH <RamizHussain500@Gmail.com>
2025-12-01 19:45:57 -0700

    Fixed Loading Screen

--- code/app/src/main/java/com/example/camaraderie/utilStuff/LoadingScreenFragment.java
+++ code/app/src/main/java/com/example/camaraderie/utilStuff/LoadingScreenFragment.java
@@ -15,24 +15,4 @@ import androidx.fragment.app.Fragment;
  */
 public class LoadingScreenFragment extends Fragment {
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
 }
